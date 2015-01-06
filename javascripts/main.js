493a50a266e4451a53a4826cbbd03817ba22efc5
Feng Ling <fling@utexas.edu>
2015-01-05 22:35:43 -0600

    tabactive

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,3 +8,12 @@ var main = function() {
 $(document).ready(main);
 
 */
+
+placenow = document.URL.split(/[\\/]/)[3];
+if(placenow == 'cv') {
+	document.getElementById("cvtab").className += "active";
+}else if(placenow == 'movrev') {
+	document.getElementById("posttab").className += "active";
+}else {
+	document.getElementById("hometab").className += "active";
+}
