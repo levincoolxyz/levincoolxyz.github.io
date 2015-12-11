9a5950dd6ea596a31cb9567315a78b09920c9390
Feng Ling <fling@utexas.edu>
2015-12-11 10:29:45 -0600

    new commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -32,3 +32,10 @@ var activatetab = function() {
 }
 
 $(document).ready(activatetab);
+
+$('.row .btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $collapse = $this.closest('.collapse-group').find('.collapse');
+    $collapse.collapse('toggle');
+});
