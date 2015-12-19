8adb75b7e8f5f23aa2277c4ad598199549274918
Feng Ling <fling@utexas.edu>
2015-12-18 23:49:40 -0600

    new commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -32,3 +32,14 @@ var activatetab = function() {
 }
 
 $(document).ready(activatetab);
+
+$("[rel='tooltip']").tooltip();    
+
+$('.thumbnail').hover(
+    function(){
+        $(this).find('.caption').slideDown(250); //.fadeIn(250)
+    },
+    function(){
+        $(this).find('.caption').slideUp(250); //.fadeOut(205)
+    }
+); 
