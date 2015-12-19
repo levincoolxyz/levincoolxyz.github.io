b6152ddffb121ca08f8be834ac74afb1498230f0
Feng Ling <fling@utexas.edu>
2015-12-19 01:23:18 -0600

    new commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -33,14 +33,7 @@ var activatetab = function() {
 
 	$("[rel='tooltip']").tooltip();    
 
-	$('.thumbnail').hover(
-	    function(){
-	        $(this).find('.caption').slideDown(250); //.fadeIn(250)
-	    },
-	    function(){
-	        $(this).find('.caption').slideUp(250); //.fadeOut(205)
-	    }
-	); 
+
 }
 
 $(document).ready(activatetab);
